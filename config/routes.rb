4ec281ac433660fb0c3af3a7f676b27ee889e0e8
Josemar Luedke <josemarluedke@gmail.com>
2014-06-19 23:10:00 +0200

    Add route scope using api constraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Neighborly::Api::Engine.routes.draw do
+  scope module: :v1,
+        constraints: Neighborly::Api::ApiConstraint.new(version: 1, default: true),
+        defaults: { format: :json } do
+    resources :projects
+  end
 end
