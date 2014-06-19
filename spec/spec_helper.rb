15a685bafb28b64deeaa7ef123bf5f3d6c2d265d
Josemar Luedke <josemarluedke@gmail.com>
2014-06-19 23:10:47 +0200

    Include rspec controller example group on spec helper
    It was not been automatically loaded

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,6 @@ ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each {|f| require f }
 
 RSpec.configure do |config|
+  config.include RSpec::Rails::ControllerExampleGroup
   config.use_transactional_fixtures = true
 end
