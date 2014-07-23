c8dd571453684241286f24879c0a4a0d893085b7
Josemar Luedke <josemarluedke@gmail.com>
2014-07-23 12:08:02 -0300

    Fix contributions between values scope when passing value with comma

--- app/models/neighborly/api/contribution.rb
+++ app/models/neighborly/api/contribution.rb
@@ -2,7 +2,9 @@ module Neighborly::Api
   class Contribution < ::Contribution
     scope :between_values, ->(start_at, ends_at) do
       return all unless start_at.present? && ends_at.present?
-      where('value between ? and ?', start_at, ends_at)
+      where('value between ? and ?',
+            start_at.to_s.sub(',', '').to_f,
+            ends_at.to_s.sub(',', '').to_f)
     end
   end
 end
